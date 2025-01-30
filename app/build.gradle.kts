2d48ace45b45d4c0d13243fb26f01ecd0ff6ab95
W-Stefanska <stefanskaweronika86@gmail.com>
2025-01-30 18:11:26 +0100

    Implementation of SQLite DB

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.transport.api)
+    implementation(libs.androidx.room.common)
+    implementation(libs.androidx.room.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/pocketjournal/MainActivity.kt
+++ app/src/main/java/com/example/pocketjournal/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.pocketjournal
 
 import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
@@ -24,12 +27,157 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.Update
 import com.example.pocketjournal.ui.theme.PocketJournalTheme
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+enum class EntryType {
+    BOOLEAN,
+    INT,
+    FLOAT,
+    STRING,
+}
+
+@Entity (tableName = "entries")
+data class Entry (
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val type: EntryType,
+    val value: String,
+    val date: Long,
+)
+
+@Dao
+interface EntryDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(entry: Entry)
+
+    @Update
+    suspend fun update(entry: Entry)
+
+    @Delete
+    suspend fun delete(entry: Entry)
+
+    @Query("SELECT * FROM entries")
+    suspend fun getAllEntries(): Flow<List<Entry>>
+
+    @Query("SELECT * FROM entries WHERE date = :date")
+    suspend fun getEntriesByDate(date: Long): Flow<List<Entry>>
+
+    @Query("SELECT * FROM entries WHERE name = :name")
+    suspend fun getEntriesByName(name: String): Flow<List<Entry>>
+
+}
+
+@Database(entities = [Entry::class], version = 1, exportSchema = false)
+abstract class EntryDatabase : RoomDatabase() {
+    abstract fun entry(): EntryDao
+
+    companion object {
+        @Volatile
+        private var Instance: EntryDatabase? = null
+
+        fun getDatabase(context: Context): EntryDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, EntryDatabase::class.java, "entry_database")
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
+
+class EntryRepository(private val entryDao: EntryDao) {
+    suspend fun insert(entry: Entry) {
+        entryDao.insert(entry)
+    }
+    suspend fun update(entry: Entry) {
+        entryDao.update(entry)
+    }
+    suspend fun delete(entry: Entry) {
+        entryDao.delete(entry)
+    }
+    suspend fun getEntries(): Flow<List<Entry>> {
+        return entryDao.getAllEntries()
+    }
+    suspend fun getEntriesByDate(date: Long): Flow<List<Entry>> {
+        return entryDao.getEntriesByDate(date)
+    }
+    suspend fun getEntriesByName(name: String): Flow<List<Entry>> {
+        return entryDao.getEntriesByName(name)
+    }
+}
+
+class EntryViewModelFactory(private val application: Application)
+    :ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return EntryViewModel(application) as T
+    }
+}
+
+class EntryViewModel(application: Application) : ViewModel() {
+    private val repository: EntryRepository
+    private val _entryState = MutableStateFlow<List<Entry>>(emptyList())
+    val entryState: StateFlow<List<Entry>>
+        get() = _entryState
+
+    init {
+        val db = EntryDatabase.getDatabase(application)
+        val dao = db.entry()
+        repository = EntryRepository(dao)
+
+        fetchEntries()
+    }
+
+    private fun fetchEntries() {
+        viewModelScope.launch {
+            repository.getEntries().collect { entry ->
+                _entryState.value = entry
+            }
+        }
+    }
+
+    fun deleteEntry(entry: Entry) {
+        viewModelScope.launch {
+            repository.delete(entry)
+        }
+    }
+
+    fun addEntry(entry: Entry) {
+        viewModelScope.launch {
+            repository.insert(entry)
+        }
+    }
+
+    fun updateEntry(entry: Entry) {
+        viewModelScope.launch {
+            repository.update(entry)
+        }
+    }
+}
 
 sealed class BottomNavScreen(val route: String, val icon: ImageVector, val label: String) {
     data object Home : BottomNavScreen("home", Icons.Default.Home, "Home")
@@ -83,16 +231,18 @@ fun BottomNavigationBar(navController: NavHostController) {
         BottomNavScreen.Settings
     )
 
-    BottomNavigation {
+    BottomNavigation (
+        modifier = Modifier.height(80.dp)
+    ){
         val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
 
         screens.forEach { screen ->
             BottomNavigationItem(
                 selected = currentRoute == screen.route,
                 onClick = { navController.navigate(screen.route) },
-                icon = { Icon(imageVector = screen.icon, contentDescription = screen.label) },
-                label = { Text(text = screen.label) },
-                modifier = Modifier.padding(8.dp)
+                icon = { Icon(imageVector = screen.icon, contentDescription = screen.label, modifier = Modifier.padding(14.dp).fillMaxSize()) },
+                //label = { Text(text = screen.label) },
+                modifier = Modifier.fillMaxSize()
             )
         }
     }
@@ -151,5 +301,13 @@ fun Settings(navController: NavHostController) {
         ) {
             Text(text = "Settings")
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CutePreview() {
+    PocketJournalTheme {
+        Navigation()
     }
 }
